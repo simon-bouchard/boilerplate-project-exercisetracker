86914ce8364be661134d7ff57ec2ab41fd1f50af
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-08 16:23:24 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const crypto = require('crypto');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +11,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const userSchema = new mongoose.Schema({
+	username: String,
+	_id: String
+});
+
+let users = mongoose.model('users', userSchema);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/users', async (req, res) => {
+	let username = req.body.username;
+	let userid = crypto.createHash('sha256').update(username + Date.now().toString()).digest('hex').slice(0, 16);
+	
+	let user = await users.create({username: username, _id: userid});
+	
+	res.json({username: username, _id: user._id})
+});
 
 
 
